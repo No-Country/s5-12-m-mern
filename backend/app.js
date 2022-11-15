688f5da0e9d7438e6502d019b3ae7f48f961f1a2
Luca Hardmeier <hardmeierluca@gmail.com>
2022-11-15 11:14:46 -0300

    mongoDB added, dotenv import modified

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,20 @@
 //Para hacer andar el node en modo --watch necesitan tener la ultima versión instalada: npm install n -g
 
 //inicialización de módulos
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
+import * as dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+import morgan from 'morgan'
+import './src/config/db.js'
 const app = express()
-const port = process.env.PORT || 3000 
+const port = process.env.PORT || 3000
 
 app.use(morgan('dev'))
 app.use(express.json()) //nuevo body parser incluido en ultimas versiones de express
-app.use(express.urlencoded({extended: false})) //necesario para metodos POST, PUT, PATCH y DELETE
+app.use(express.urlencoded({ extended: false })) //necesario para metodos POST, PUT, PATCH y DELETE
 
 //rutas
 app.use('/api', require('./src/routes/index')) //base donde están todas las rutas
 
-app.get('*', (req, res) => res.status(404).json({error: "Not found"})) //msg de error 404 para rutas no definidas
+app.get('*', (req, res) => res.status(404).json({ error: "Not found" })) //msg de error 404 para rutas no definidas
 app.listen(port, () => console.log(`App working on port ${port}!`))
