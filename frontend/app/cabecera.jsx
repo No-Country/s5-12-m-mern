4c3dfd620b06c7c0cc142f473d5c06352e2b93cc
Felipe <felipe.calderon321@gmail.com>
2022-11-14 22:31:41 -0300

    setting base structure to Frontend develpment

---
+++ frontend/app/cabecera.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>App para mascotas</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Mascotas app" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
