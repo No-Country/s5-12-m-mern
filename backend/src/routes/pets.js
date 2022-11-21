9b751632504bea2ce216d584a03c9aeca3075a94
malks14 <73920001+malks14@users.noreply.github.com>
2022-11-21 19:12:40 -0300

    feat: se agrego rutas id, put y delete

--- backend/src/routes/pets.js
+++ backend/src/routes/pets.js
@@ -1,10 +1,13 @@
 
 import express from "express"
-import { getPets, createPet } from "../controllers/pets.js"
+import { getPets, getPetById,createPet, updatePet, deletePet } from "../controllers/pets.js"
 
 const petRoutes = express.Router()
 
 petRoutes.get('/pets', getPets)
+petRoutes.get('/pets/:petId', getPetById)
 petRoutes.post('/pets', createPet)
+petRoutes.put('/pets/:petId', updatePet);
+petRoutes.delete('/pets/:petId', deletePet);
 
 export default petRoutes
