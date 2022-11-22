9a18fb8cbc14deb8992d2be8634d948292524ca2
Luca Hardmeier <hardmeierluca@gmail.com>
2022-11-22 11:34:08 -0300

    db connect modified

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 const connectMongoDb = async () => {
     try {
-        await mongoose.connect(`${process.env.DB_URI}`)
+        await mongoose.connect(process.env.DB_URI, { dbName: "walkncare" })
         console.log('Connected to MongoDB')
     } catch (err) {
         throw Error(err);
