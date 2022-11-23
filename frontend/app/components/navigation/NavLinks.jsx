e425a682175e85adf0b41acf73e092614d4ff718
malks14 <73920001+malks14@users.noreply.github.com>
2022-11-23 18:24:27 -0300

    fix: nuevo componenete navlinks

--- frontend/app/components/navigation/NavLinks.jsx
+++ frontend/app/components/navigation/NavLinks.jsx
@@ -1,37 +1,75 @@
+"use client";
+
+import { useState } from "react";
 import Link from "next/link";
-import styles from "./NavLinks.module.css"
-import {AiOutlineUser, AiOutlineBell} from "react-icons/ai"
+import styles from "./NavLinks.module.css";
+import { AiOutlineUser, AiOutlineBell } from "react-icons/ai";
+import { BsThreeDots, BsClockHistory } from "react-icons/bs";
+import { FaPaw, FaSignOutAlt, FaSignInAlt } from "react-icons/fa";
+import Backdrop from "../UI/backdrop/Backdrop";
+import SideDrawer from "./SideDrawer";
+import { useContext } from "react";
+import AuthContext from "../../../store/auth-context";
 
 const NavLinks = () => {
+  const authCtx = useContext(AuthContext);
+  console.log(authCtx);
+
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const openDrawerHandler = () => {
+    setIsDrawerOpen(true);
+  };
+
+  const closeDrawerHandler = () => {
+    setIsDrawerOpen(false);
+  };
+
+  const logoutHandler = () => {
+    authCtx.logout()
+  }
+
   return (
-    <ul className={styles["nav-links"]}>
-      <li>
-        <Link href="/fdfdfd">
-          Home
-        </Link>
-      </li>
-
-      <li>
-        <Link href="/fdfdfdf">
-          Mascotas
-        </Link>
-      </li>
-
-      <li>
-        <Link href="/fdfdfd">
-          Nosotros
-        </Link>
-      </li>
-
-      <li>
-        <Link href="/fdfdfd">
+    <>
+      {isDrawerOpen && <Backdrop onClick={closeDrawerHandler} />}
+      <ul className={styles["nav-links"]}>
+        <li>
           <AiOutlineUser />
-        </Link>
-      </li>
-      <li>
-        <AiOutlineBell />
-      </li>
-    </ul>
+        </li>
+        <li>
+          <AiOutlineBell />
+        </li>
+        <li>
+          <button onClick={openDrawerHandler}>
+            <BsThreeDots />
+          </button>
+        </li>
+        {isDrawerOpen && (
+          <div className={styles.drawerCtn}>
+            <ul>
+              <li>
+                Historial <BsClockHistory />
+              </li>
+              <li>Quiero ser paseador cuidador</li>
+              <li>
+                Perfil mascota <FaPaw />
+              </li>
+              {!authCtx.isLoggedIn ? (
+                <li>
+                  <Link href="/login">Iniciar sesión</Link>
+                  <FaSignInAlt />
+                </li>
+              ) : (
+                <li>
+                  <button onClick={logoutHandler}>Cerrar sesión</button>
+                  <FaSignOutAlt />
+                </li>
+              )}
+            </ul>
+          </div>
+        )}
+      </ul>
+    </>
   );
 };
 
