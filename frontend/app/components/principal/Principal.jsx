fa671755f538c23c102c4cd1f23f171c429a5b57
ezSan <ezequiel.sanchez.7391@gmail.com>
2022-11-23 18:07:17 -0300

    paseador section created and styled

--- frontend/app/components/principal/Principal.jsx
+++ frontend/app/components/principal/Principal.jsx
@@ -2,11 +2,13 @@
 import React from "react";
 import styles from "./principal.module.css";
 import ClientWithButton from "./ClientWithButton";
+import PaseadorWithButton from "./PaseadorWithButton";
 
 const { principalSection } = styles;
 
 export default function Principal() {
   return <div className={principalSection}>
     <ClientWithButton/>
+    <PaseadorWithButton/>
   </div>;
 }
