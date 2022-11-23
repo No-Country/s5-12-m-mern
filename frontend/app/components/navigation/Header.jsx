40c9a4314615314b7f32e5c2bfbfcc05a225921f
malks14 <73920001+malks14@users.noreply.github.com>
2022-11-23 18:24:05 -0300

    fix: nuevo header

--- frontend/app/components/navigation/Header.jsx
+++ frontend/app/components/navigation/Header.jsx
@@ -1,39 +1,16 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import Backdrop from "../UI/backdrop/Backdrop";
 import NavLinks from "./NavLinks";
-import SideDrawer from "./SideDrawer";
-import { GiHamburgerMenu } from "react-icons/gi";
 import styles from "./Header.module.css";
 
 const Header = () => {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
-  const openDrawerHandler = () => {
-    setIsDrawerOpen(true);
-  };
-
-  const closeDrawerHandler = () => {
-    setIsDrawerOpen(false);
-  };
   return (
     <>
-      {isDrawerOpen && <Backdrop onClick={closeDrawerHandler} />}
-      <SideDrawer show={isDrawerOpen} onClick={closeDrawerHandler}>
-        <nav className={styles["main-navigation__drawer-nav"]}>
-          <NavLinks />
-        </nav>
-      </SideDrawer>
       <div className={styles["header-ctn"]}>
-        <button
-          onClick={openDrawerHandler}
-          className={styles["main-navigation__menu-btn"]}
-        >
-          <GiHamburgerMenu size={25} />
-        </button>
         <Link href="/" className={styles.ctnInfoNav}>
           <Image
             src="/logowc.png"
