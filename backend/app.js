a5ca3ec8d65b816a188483e3f94b9974123836a2
malks14 <73920001+malks14@users.noreply.github.com>
2022-11-23 18:22:09 -0300

    fix: solucion cors temp

--- backend/app.js
+++ backend/app.js
@@ -15,17 +15,27 @@ import petRoutes from './src/routes/pets.js';
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(cors({
-    'origin': ['*'], //cambiar * por ip donde se alojará la app front
-    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-}));
+// app.use(cors({
+//     'origin': ['*'], //cambiar * por ip donde se alojará la app front
+//     'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+// }));
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  
+    next();
+  });
 app.use(morgan('dev'))
 app.use(express.json()) 
 app.use(express.urlencoded({ extended: false }))
 
 //rutas
 // app.use('/api/pets', hasToken, petsRouter)
-app.use('/api', petRoutes)
+app.use('/api/pets', petRoutes)
 app.use('/api/user', userRouter)
 
 app.get('*', (req, res) => res.status(404).json({ error: "Not found" }))
