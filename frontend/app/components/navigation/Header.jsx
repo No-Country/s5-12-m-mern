5b9172b01493d9417f6ac10a3e42084f90cabda1
malks14 <73920001+malks14@users.noreply.github.com>
2022-11-24 15:16:30 -0300

    feat: nav celualr

--- frontend/app/components/navigation/Header.jsx
+++ frontend/app/components/navigation/Header.jsx
@@ -1,16 +1,34 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import NavLinks from "./NavLinks";
+import Backdrop from "../UI/backdrop/Backdrop";
+import { GiHamburgerMenu } from "react-icons/gi";
 import styles from "./Header.module.css";
+import SideDrawer from "./SideDrawer";
 
 const Header = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
+  const openDrawerHandler = () => {
+    setIsDrawerOpen(true);
+  };
+
+  const closeDrawerHandler = () => {
+    setIsDrawerOpen(false);
+  };
   return (
     <>
+      {isDrawerOpen && <Backdrop onClick={closeDrawerHandler} />}
+      <SideDrawer show={isDrawerOpen} onClick={closeDrawerHandler}>
+        <nav className={styles["main-navigation__drawer-nav"]}>
+          <NavLinks />
+        </nav>
+      </SideDrawer>
       <div className={styles["header-ctn"]}>
+
         <Link href="/" className={styles.ctnInfoNav}>
           <Image
             src="/logowc.png"
@@ -21,7 +39,12 @@ const Header = () => {
 
           <h2>Walk&Care</h2>
         </Link>
-
+        <button
+          onClick={openDrawerHandler}
+          className={styles["main-navigation__menu-btn"]}
+        >
+          <GiHamburgerMenu size={25} />
+        </button>
         <nav className={styles["main-navigation__header-nav"]}>
           <NavLinks />
         </nav>
