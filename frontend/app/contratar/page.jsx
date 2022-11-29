07193f24bf32fe57e6b2c08a51272f99fb36a26f
ezSan <ezequiel.sanchez.7391@gmail.com>
2022-11-29 12:05:50 -0300

    filter button addded

--- frontend/app/contratar/page.jsx
+++ frontend/app/contratar/page.jsx
@@ -1,6 +1,12 @@
+'use client'
 import React from "react";
 import styles from "./contratar.module.css";
 import PaseadorCard from "./PaseadorCard";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 
 const { title, cardContainer } = styles;
 
@@ -8,6 +14,21 @@ export default function Contratar() {
   return (
     <div>
       <h1 className={title}>QUIERO CONTRATAR</h1>
+      <div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          Ordenar por 
+        </AccordionSummary>
+        <AccordionDetails>
+            <p>Mayor precio</p>
+            <p>Menor precio</p>
+        </AccordionDetails>
+      </Accordion>
+      </div>
       <div className={cardContainer}>
         <PaseadorCard />
         <PaseadorCard />
