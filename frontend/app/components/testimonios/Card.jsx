eace9bb0c8dec65e65cb370a40ea738eaaef17e6
Felipe <felipe.calderon321@gmail.com>
2022-12-07 07:40:39 -0300

    testimonios responsive con tailwindcss

--- frontend/app/components/testimonios/Card.jsx
+++ frontend/app/components/testimonios/Card.jsx
@@ -2,20 +2,23 @@
 import React from "react";
 import styles from "./card.module.css";
 import Image from "next/image";
-import avatar from "../../../public/avatar.png";
+import avatar from "../../../public/profile1.jpg";
 import RatingStart from "./RatingStart";
 
-const { card, outOfCardImage, cardTitle, cardContent } = styles;
+const { card } = styles;
 
 export default function Card() {
   return (
-    <div className={card}>
-      <Image src={avatar} alt="testimonios" />
-      <p>
-        "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
-        consectetur, adipisci velit..."
-      </p>
-      <RatingStart />
+    <div className="max-w-sm bg-principal rounded-lg shadow-sm hover:bg-[#f9df98] hover:shadow-xl transition-all">
+    <Image src={avatar} alt="testimonios" className="mx-auto w-1/4 rounded-full mt-3"/>
+
+    <div className="p-5">
+      <p className="mb-3 font-normal text-gray-700 text-center">"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</p>
+      <h6 className="mb-2 text-xl italic tracking-tight text-gray-900 text-center">Pedrito Pérez</h6>
+      <div className="text-center">
+        <RatingStart />
+      </div>
+    </div>
     </div>
   );
 }
--- frontend/app/components/testimonios/Testimonios.jsx
+++ frontend/app/components/testimonios/Testimonios.jsx
@@ -13,11 +13,10 @@ const Testimonios = () => {
 
   
   return (
-    <div className={testimoniosCtn}>
-      <h2 className={title}>NUESTROS CLIENTES NOS RECOMIENDAN</h2>
+    <div className="bg-secundario pt-6 pb-12">
+      <h2 className="text-center text-3xl pt-3 pb-6 text-principal">NUESTROS CLIENTES NOS RECOMIENDAN</h2>
 
-      <div className={cardsContainer}>
-        <Card/>
+      <div className="flex flex-wrap justify-center gap-6">
         <Card />
         <Card />
         <Card />
